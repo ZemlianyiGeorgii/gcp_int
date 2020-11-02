035b0048d910c2cc5ebeead97fd272c4e99b955e
georgii.zemlianyi <georgii.zemlianyi@extendaretail.com>
2020-11-02 16:43:40 +0200

    feat: Add new manual job 11

--- script/my_script.py
+++ script/my_script.py
@@ -1,6 +1,21 @@
 import sys
-print (sys.argv[0])
-print (sys.argv[1])
-print(
-    "Updated dataset '{}' with modified user permissions."
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+dataset_id = sys.argv[1]
+
+dataset = client.get_dataset(dataset_id)  # Make an API request.
+
+entry = bigquery.AccessEntry(
+    role='WRITER',
+    entity_type='userByEmail',
+    entity_id=' hii-rra-datawarehouse-loader@analytics-staging-e1b8.iam.gserviceaccount.com',
 )
+
+entries = list(dataset.access_entries)
+entries.append(entry)
+dataset.access_entries = entries
+
+dataset = client.update_dataset(dataset, ['access_entries'])
